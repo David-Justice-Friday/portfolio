7c1fa5d4815b11052e91441299bf2f4527658fe1
David-Justice-Friday <davidjusticefridayjf@gmail.com>
2023-10-19 12:16:58 +0100

    c

---
+++ waste.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
